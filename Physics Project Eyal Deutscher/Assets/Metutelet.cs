9768c71bcd08042f87297c58bc69f42618a26eee
EDeyal <75429849+EDeyal@users.noreply.github.com>
2024-01-15 16:56:06 +0200

    Metutelet moving on positive area only

--- Physics Project Eyal Deutscher/Assets/Metutelet.cs
+++ Physics Project Eyal Deutscher/Assets/Metutelet.cs
@@ -1,3 +1,6 @@
+using System;
+using Unity.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Metutelet : MonoBehaviour
@@ -5,14 +8,16 @@ public class Metutelet : MonoBehaviour
     [SerializeField] Transform _pillarTop;
     [SerializeField] float _maxRadius;
     [SerializeField] bool _isRightDirection;
-    [SerializeField] float _startingHight;
     [SerializeField] float _acceleration = 0;//starting with no acceleration
     [SerializeField] bool _isGoingUp = false;
     [SerializeField] float _speedFactor = 1;
-
+    [SerializeField] float _gravity = 9.81f;
+    [SerializeField] float _moveToSidemount = 0.001f;
+    [Header("Do Not Edit")]
+    [SerializeField] float _startingHight;
     //use physics to make the ball drop down, you must keep the ball at a regular distance from the top part
     //when the ball is too far move it closer to the center, if it is too close move it to the current side
-    //When the ball swings to the right side, it will go up 
+    //When the ball swings to the right side, it will go right if it goes up it will go up
     //make ball have acceleration and decceleration
     private void Start()
     {
@@ -28,9 +33,20 @@ public class Metutelet : MonoBehaviour
             this.transform.position += new Vector3(0, _acceleration, 0);
         }
         else
-        { 
+        {
             //going down
             this.transform.position -= new Vector3(0, _acceleration, 0);
+        }
+        //move the ball left or right
+        if (_isRightDirection)
+        {
+            //going right
+            this.transform.position += new Vector3(+_moveToSidemount, 0, 0);
+        }
+        else
+        {
+            //going left
+            this.transform.position -= new Vector3(-_moveToSidemount,0, 0);
         }
 
         //Adjust Acceleration
@@ -53,12 +69,29 @@ public class Metutelet : MonoBehaviour
             //change dicrection
             if (_isRightDirection)
             {
-                _isRightDirection = false;
+                if (this.transform.position.x > _pillarTop.position.x)
+                    _isRightDirection = false;
             }
             else
             {
-                _isRightDirection = true;
+                if (this.transform.position.x < _pillarTop.position.x)
+                    _isRightDirection = true;
             }
+        }
+        //set direction ball is going to
+        if (_isGoingUp)
+        {
+            if (this.transform.position.y > _pillarTop.position.y)
+                _isGoingUp = false;
+        }
+        else
+        {
+            if (this.transform.position.y < (_pillarTop.position.y - _maxRadius))
+            {
+                _isGoingUp = true;
+                this.transform.position = new Vector3(transform.position.x, (_pillarTop.position.y - _maxRadius), transform.position.z);
+            }
+
         }
         //Adjust Ball position
         //Position 1 = Ball, Position 2 = Pillar Top
@@ -71,27 +104,48 @@ public class Metutelet : MonoBehaviour
         if (distance > _maxRadius)
         {
             //too far, need to move closer
-            if (_isRightDirection)
-            {
-                //move right
-            }
-            else
-            {
-                //move left
-            }
+            float x1Calculation;
+            float d = _maxRadius * _maxRadius;//D^2
+            float yValue = this.transform.position.y - _pillarTop.transform.position.y;//y1-y2
+            yValue = yValue * yValue;//(y1-y2)^2
+            x1Calculation = MathF.Sqrt(d - yValue);//Root(D^2-(y1-y2)^2)
+            x1Calculation = x1Calculation + _pillarTop.transform.position.x;//Root(D^2-(y1-y2)^2) + x2
+                                                                            //set X
+            this.transform.position = new Vector3(x1Calculation, transform.position.y, transform.position.z);
         }
         else if (distance < _maxRadius)
         {
             //too close, need to move farther
-            if (_isRightDirection)
+            float x1Calculation;
+            float d = _maxRadius * _maxRadius;//D^2
+            float yValue = this.transform.position.y - _pillarTop.transform.position.y;//y1-y2
+            yValue = yValue * yValue;//(y1-y2)^2
+            x1Calculation = MathF.Sqrt(d - yValue);//Root(D^2-(y1-y2)^2)
+            x1Calculation = x1Calculation + _pillarTop.transform.position.x;//Root(D^2-(y1-y2)^2) + x2
+                                                                            //set X
+            this.transform.position = new Vector3(x1Calculation, transform.position.y, transform.position.z);
+        }
+        //Set Ball Direction with acceleration
+        if (_isGoingUp)
+        {
+            _acceleration -= _gravity;
+        }
+        else
+        {
+            _acceleration += _gravity;
+        }
+        if (_acceleration <= 0)
+        {
+            //change direction
+            if (_isGoingUp)
             {
-                //move left
+                _isGoingUp = false;
             }
             else
             {
-                //move right
+                _isGoingUp = true;
             }
         }
-        //Set Ball Direction with acceleration
+
     }
 }
