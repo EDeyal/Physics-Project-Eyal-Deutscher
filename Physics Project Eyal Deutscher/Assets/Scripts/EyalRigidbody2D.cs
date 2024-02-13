829dd86d023f847b29d62904ab540948c67808e2
EDeyal <75429849+EDeyal@users.noreply.github.com>
2024-02-13 22:17:23 +0200

    Rigidbody improvements

--- Physics Project Eyal Deutscher/Assets/Scripts/EyalRigidbody2D.cs
+++ Physics Project Eyal Deutscher/Assets/Scripts/EyalRigidbody2D.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class EyalRigidbody2D : MonoBehaviour
 {
     [SerializeField] float _mass;
-    [SerializeField] float _friction;
+    [SerializeField] float _drag;
     [SerializeField] bool _hasGravity;
     [SerializeField] Vector2 _gravityForce;
+
+    [Header("Borders")]
+    [SerializeField]float _yTop;
+    [SerializeField]float _yBottom;
+    [SerializeField]float _xRight;
+    [SerializeField]float _xLeft;
     [Header("Do not Change, read only fields")]
-    [SerializeField] float _acceleration;
-    [SerializeField] Vector2 _currentMovementForce;
+    [SerializeField] Vector2 _accelerationForce;
+    [SerializeField] Vector2 _velocity;
     [SerializeField]List<Vector2> _activeForces;
     [SerializeField] List<Vector2> _nextActiveForces;
 
-    private void AddForce(Vector2 force)
+    private void Awake()
+    {
+        if (_mass == 0)
+            throw new System.Exception($"Mass of Object{this.gameObject.name} is 0");
+    }
+    public void AddForce(Vector2 force)
     {
         _activeForces.Add(force);
     }
@@ -22,16 +34,19 @@ public class EyalRigidbody2D : MonoBehaviour
     {
         _activeForces.Clear();
     }
-    private void CalculateCurreMovementForce()
+    private void CalculateVelocity()
     {
-        _currentMovementForce = Vector2.zero;
-        foreach (var vector in _activeForces)
-        {
-            _currentMovementForce += vector;
-        }
         if (_hasGravity)
         {
-            _currentMovementForce += _gravityForce;
+            _accelerationForce += _gravityForce * _mass *Time.fixedDeltaTime;
+        }
+        _velocity += _accelerationForce*Time.fixedDeltaTime;
+    }
+    private void CalculateAccelerationForce()
+    {
+        foreach (var force in _activeForces)
+        {
+            _accelerationForce += force;
         }
     }
     private void ReduceForcesEffects()
@@ -41,26 +56,26 @@ public class EyalRigidbody2D : MonoBehaviour
             float x = _activeForces[i].x;
             if (x > 0)
             {
-                x -= _friction * Time.fixedDeltaTime;
+                x -= _drag * Time.fixedDeltaTime;
                 if (x < 0)
                     x = 0;
             }
             else if (x < 0)
             {
-                x += _friction * Time.fixedDeltaTime;
+                x += _drag * Time.fixedDeltaTime;
                 if (x > 0)
                     x = 0;
             }
             float y = _activeForces[i].y;
             if (y > 0)
             {
-                y -= _friction * Time.fixedDeltaTime;
+                y -= _drag * Time.fixedDeltaTime;
                 if (y < 0)
                     y = 0;
             }
             else if (y < 0)
             {
-                y += _friction * Time.fixedDeltaTime;
+                y += _drag * Time.fixedDeltaTime;
                 if (y > 0)
                     y = 0;
             }
@@ -72,13 +87,48 @@ public class EyalRigidbody2D : MonoBehaviour
             }
             _nextActiveForces.Add(_activeForces[i]);
         }
-        _activeForces = _nextActiveForces;
+        TransferNextForcesToActiveForces();
+    }
+    private void TransferNextForcesToActiveForces()
+    {
+        _activeForces.Clear();
+        foreach (var force in _nextActiveForces)
+        {
+            _activeForces.Add(force);
+        }
         _nextActiveForces.Clear();
+    }
+    private void CheckBorders()
+    {
+
+        if (this.transform.position.y >= _yTop)
+        {
+            _velocity *= new Vector2(1, -1);
+            transform.position = new Vector2(transform.position.x, _yTop);
+        }
+        if (this.transform.position.y <= _yBottom)
+        {
+            _velocity *= new Vector2(1, -1);
+            transform.position = new Vector2(transform.position.x, _yBottom);
+        }
+        if (this.transform.position.x >= _xRight)
+        {
+            _velocity *= new Vector2(-1, 1);
+            transform.position = new Vector2(_xRight, transform.position.y);
+
+        }
+        if (this.transform.position.x <= _xLeft)
+        {
+            _velocity *= new Vector2(-1, 1);
+            transform.position = new Vector2(_xLeft, transform.position.y);
+        }
     }
     public void FixedUpdate()
     {
-        CalculateCurreMovementForce();
-        transform.Translate(_currentMovementForce.x * Time.fixedDeltaTime, _currentMovementForce.y * Time.fixedDeltaTime, 0);
+        CalculateAccelerationForce();
+        CalculateVelocity();
+        transform.Translate(_velocity.x * Time.fixedDeltaTime / _mass, _velocity.y * Time.fixedDeltaTime / _mass, 0);
         ReduceForcesEffects();
+        CheckBorders();
     }
 }
